09f848511ed7961badc659060a1a81b76edf3827
lvcky <mail@johnodonnell.xyz>
2026-05-11 14:08:17 -0400

    push

--- basics/first_project/main_three.cpp
+++ basics/first_project/main_three.cpp
@@ -7,7 +7,9 @@ int main()
 
     int myInt;
     myInt = 15;
+    double myDouble = 3.14;
 
     cout << "The value of myInt is: " << myInt << endl;
+    cout << "The value of myDouble is: " << myDouble << endl;
     return 0;
 }
